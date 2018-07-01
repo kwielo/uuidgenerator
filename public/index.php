742d97557615737f9e2e5b8893e73bf990a0eb89
Krzysztof <kwielogorski@gmail.com>
2018-07-01 19:03:17 +0200

    prod env

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../src/App/Kernel.php';
 
-$kernel = new \App\Kernel('dev', true);
+$kernel = new \App\Kernel('prod', false);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
